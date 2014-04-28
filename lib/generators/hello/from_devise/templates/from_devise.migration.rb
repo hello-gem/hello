41bffd287d456c9d03ec37c91589e2d98a6b6c7a
Thiago Pinto <tapgyn@gmail.com>
2014-04-28 15:22:10 -0400

    rails g hello:from_devise

---
+++ lib/generators/hello/from_devise/templates/from_devise.migration.rb
@@ -0,0 +1,35 @@
+# customize this migration if you need, PRs accepted
+class FromDevise < ActiveRecord::Migration
+  def up
+    from_users_to_identities
+    remove_devise_columns
+  end
+
+  def down
+    raise "you can't roll this back, use a backup"
+  end
+
+  private
+
+      def from_users_to_identities
+        puts "before Identity.count (#{Identity.count.to_s.red})"
+        User.find_each do |user|
+          puts "starting User ##{user.id} #{user.email}"
+          username = user.username rescue ""
+          identity_fields = {email: user.email, username: username, password: SecureRandom.hex(8)}
+          user.identities.strategy_password.create!(identity_fields)
+        end
+        puts "after Identity.count (#{Identity.count.to_s.green})"
+      end
+
+      def remove_devise_columns
+        columns = [:username, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at]
+        columns.each { |column_name| remove_users_column_safe(column_name) }
+      end
+
+          def remove_users_column_safe(column_name)
+            return if not column_exists?(:users, column_name)
+            remove_column(:users, column_name)
+          end
+
+end
