94c7d2e7f0417d9b017a5754bab47fac5f88dcbe
Thiago Pinto <tapgyn@gmail.com>
2014-05-10 00:53:47 -0400

    username field might not exist

--- lib/generators/hello/from_devise/templates/from_devise.migration.rb
+++ lib/generators/hello/from_devise/templates/from_devise.migration.rb
@@ -15,12 +15,17 @@ class FromDevise < ActiveRecord::Migration
         puts "before Credential.count (#{Credential.count.to_s.red})"
         User.find_each do |user|
           puts "starting User ##{user.id} #{user.email}"
-          username = user.username rescue ""
+          username = extract_username(user)
           credential_fields = {email: user.email, username: username, password: SecureRandom.hex(8)}
-          user.credentials.strategy_password.create!(credential_fields)
+          user.credentials.classic.create!(credential_fields)
         end
         puts "after Credential.count (#{Credential.count.to_s.green})"
       end
+
+          def extract_username(user)
+            username = user.try(:username).to_s
+            username = username.gsub('.', '_dot_')
+          end
 
       def remove_devise_columns
         columns = [:username, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at]
