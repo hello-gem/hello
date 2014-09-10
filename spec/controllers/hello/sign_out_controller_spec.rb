15d4cbd4d177d46fdd76b95290e2ecb53b7b2151
Thiago Pinto <tapgyn@gmail.com>
2014-09-09 22:35:28 -0300

    improved sign out controller spec

--- spec/controllers/hello/sign_out_controller_spec.rb
+++ spec/controllers/hello/sign_out_controller_spec.rb
@@ -5,19 +5,26 @@ module Hello
     routes { Hello::Engine.routes }
 
     # POST /hello/sign_out
-    describe "POST sign_up" do
-      describe "works" do
+    describe "GET sign_out" do
 
-        it "Destroyed" do
-          delete :sign_out, {format: :json}
-          
-          expect(response.body).to eq('')
-          expect(response.status).to eq(205)
-          expect(response.status_message).to eq("Reset Content")
-          expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
-        end
-
+      it "Signed Out" do
+        get :sign_out, {format: :json}
       end
+
+      it "Signed In" do
+        s = given_I_have_a_classic_active_session
+        expect(ActiveSession.count).to eq(1)
+        get :sign_out, {format: :json, access_token: s.access_token}
+      end
+
+      after do
+        expect(response.body).to eq('')
+        expect(response.status).to eq(205)
+        expect(response.status_message).to eq("Reset Content")
+        expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
+        expect(ActiveSession.count).to eq(0)
+      end
+
     end
 
   end
