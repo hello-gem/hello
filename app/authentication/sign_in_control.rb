72725a7a0ef0f71597a539708d4286405813b72a
Thiago Pinto <tapgyn@gmail.com>
2015-02-14 01:38:13 -0200

    improving code readibility

--- app/authentication/sign_in_control.rb
+++ app/authentication/sign_in_control.rb
@@ -3,7 +3,6 @@ class SignInControl < Hello::AbstractControl
   alias :sign_in :entity
 
   def success
-    expires_at     = !!c.params[:keep_me] ? 30.days.from_now : 30.minutes.from_now
     access_token = c.create_hello_access_token(expires_at)
 
     c.respond_to do |format|
@@ -20,6 +19,19 @@ class SignInControl < Hello::AbstractControl
       format.json { c.render json: sign_in.errors, status: :unprocessable_entity }
     end
   end
+
+
+
+  private
+
+  def expires_at
+    if c.params[:keep_me]
+      30.days.from_now
+    else
+      30.minutes.from_now
+    end
+  end
+
 
 
 end
