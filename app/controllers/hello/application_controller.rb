0b944332f715391bc3b4403bded11ae6a4f9455c
Thiago Pinto <tapgyn@gmail.com>
2015-02-14 01:35:21 -0200

    removing code duplication

--- app/controllers/hello/application_controller.rb
+++ app/controllers/hello/application_controller.rb
@@ -5,20 +5,24 @@
 class Hello::ApplicationController < ApplicationController
 
   rescue_from Hello::JsonNotSupported do |exception|
-    data = {
-      maintenance: false,
-      action:      "#{controller_name}##{action_name}",
-      exception: {
-        class:       exception.class.name,
-        message:     exception.message
-      }
-    }
-
-    render json: data, status: :bad_request
+    render json: _json_data_for_exception(exception), status: :bad_request
   end
 
   rescue_from ActionController::ParameterMissing do |exception|
-    data = {
+    respond_to do |format|
+      format.html { raise exception }
+      format.json { render json: _json_data_for_exception(exception), status: :bad_request } # 400
+    end
+  end
+
+
+
+
+  private
+
+  # Don't override this at home, kids
+  def _json_data_for_exception(exception)
+    {
       maintenance: false,
       action:      "#{controller_name}##{action_name}",
       exception: {
@@ -27,11 +31,6 @@ class Hello::ApplicationController < ApplicationController
         # backtrace:   exception.backtrace
       }
     }
-
-    respond_to do |format|
-      format.html { raise exception }
-      format.json { render json: data, status: :bad_request } # 400
-    end
   end
 
 end
