fbb7f8ec8d9f523251fc5459792774aed63da3b2
Thiago Pinto <tapgyn@gmail.com>
2015-02-18 03:52:35 -0200

    removing unused code

--- lib/generators/hello/controls/controls_generator.rb
+++ lib/generators/hello/controls/controls_generator.rb
@@ -3,9 +3,7 @@ class Hello::ControlsGenerator < Rails::Generators::Base
   source_root File.expand_path("../../../../../", __FILE__)
 
   def copy_the_controls
-    # the_root = File.expand_path("../../../../../", __FILE__)
-    # destination = 
-    directory "app/authentication" #, destination
+    directory "app/authentication"
   end
 
 
