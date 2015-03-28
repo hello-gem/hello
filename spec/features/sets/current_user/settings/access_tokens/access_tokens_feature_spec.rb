0e488e1be7c49158a989ff2b01f7d55dba12b329
Thiago Pinto <tapgyn@gmail.com>
2015-03-27 21:44:24 -0300

    Rewriting specs: access token feature set

---
+++ spec/features/sets/current_user/settings/access_tokens/access_tokens_feature_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+RSpec.describe "Top Feature Set: Current User", :type => :feature do
+  context "Feature Set: Settings" do
+    feature "Feature Set: Access Tokens" do
+
+
+
+
+
+      feature "Feature: Unlink" do
+        
+        scenario "Scenario: Success" do
+          given_I_have_signed_in_with_sudo_mode
+
+          Given "I have a second device signed in" do
+            create(:access_token, user: User.last)
+          end
+
+          Given "I go to Settings > Access Tokens" do
+            click_link "Settings"
+            click_link "Access Tokens (2)"
+          end
+
+          When "I click 'Unlink'" do
+            click_button "Unlink"
+          end
+
+          Then "I should see a confirmation message" do
+            expect_flash_notice("Device has been unlinked from your account")
+          end
+
+          Then "I should only see one access token" do
+            then_I_should_see("Access Tokens (1)")
+          end
+        end
+
+        scenario "Scenario: Failure" do
+          skip "DestroyAccessTokenEntity#alert_message"
+        end
+
+      end
+
+      feature "Feature: Unlink" do
+
+        scenario "Scenario: Success" do
+          skip "TODO"
+        end
+
+        scenario "Scenario: Failure" do
+          skip "TODO"
+        end
+      end
+
+
+      
+    end
+  end
+end
