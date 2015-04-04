3084f5614c714858209a9dcc39968b372cd1a28a
Thiago Pinto <tapgyn@gmail.com>
2015-04-04 19:09:12 -0300

    fixing generator template

--- lib/generators/hello/install/templates/credential.rb
+++ lib/generators/hello/install/templates/credential.rb
@@ -1,11 +1,6 @@
 class Credential < ActiveRecord::Base
   include Hello::CredentialModel # keep this line for gem hello
 
-  # don't want usernames?
-  # just comment out the line below
-  # we strongly recommend the field exists in case your CEO ever ever ever changes their mind
-  validates_presence_of :username
-
   # specify what happens to associated records when the user decided to terminate their account
   # has_many :things_to_destroy,  dependent: :destroy
   # has_many :things_to_nulify,   dependent: :nullify
--- lib/generators/hello/install/templates/user.rb
+++ lib/generators/hello/install/templates/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   include Hello::UserModel # keep this line for gem hello
 
+  # don't want usernames?
+  # just comment out the line below
+  # we strongly recommend the field exists in case your CEO ever ever ever changes their mind
+  validates_presence_of :username
+
   # specify what happens to associated records when the user decided to terminate their account
   # has_many :things_to_destroy,  dependent: :destroy
   # has_many :things_to_nulify,   dependent: :nullify
