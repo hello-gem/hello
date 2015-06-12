d546783307908f970020892bd3bb4840c3046ff7
Thiago Pinto <tapgyn@gmail.com>
2015-06-12 09:41:10 -0300

    Reviewing Features: Manage Profile and Updating Password

---
+++ spec/bdd/hello/account_management/manage_password/manage_password_page_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+RSpec.describe "Hello Gem", type: :feature do
+  goal_feature "Account Management", "Manage Password", "Password Page" do
+
+
+
+    def self._before__given_I_am_on_the_password_page
+      before do
+        Given "I am on the Password Management Page" do
+          given_I_have_signed_in
+          click_link "Settings"
+          click_link "Password"
+          expect(current_path).to eq hello.user_password_path
+        end
+      end
+    end
+
+    
+
+    sstory "Update Password" do
+      _before__given_I_am_on_the_password_page
+
+
+
+      sscenario "Valid" do
+        When "I submit a new valid password" do
+          fill_in 'user_password', with: (@new_password = 'new_password')
+          click_button 'Update'
+        end
+
+
+
+        Then "I should see a confirmation message" do
+          expect_flash_notice "You have updated your profile successfully"
+        end
+
+
+
+        Then "and I should be able to sign in with the new password" do
+          click_link "Sign Out"
+          when_sign_in_with_standard_data(password: @new_password)
+          expect_flash_notice "You have signed in successfully"
+        end
+      end
+
+
+
+      sscenario "Invalid" do
+        When "I submit an invalid password" do
+          fill_in 'user_password', with: ''
+          click_button 'Update'
+        end
+
+
+
+        Then "I should see an alert message" do
+          expect_error_message "1 error was found while updating your profile"
+        end
+      end
+    end
+
+
+
+  end
+end
--- spec/bdd/hello/account_management/manage_profile/manage_profile_pending_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe "Hello Gem", type: :feature do
-  goal_feature "Account Management", "Manage Password", "-" do
-
-    pending
-
-  end
-end
