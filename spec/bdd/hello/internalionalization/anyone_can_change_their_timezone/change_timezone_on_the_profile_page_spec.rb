7d5d206d94bb5a945328b0bf8726dc0f771ab01d
James Pinto <thejamespinto@gmail.com>
2015-07-23 20:06:34 -0300

    Templating Feature: Anyone Can Change Their Timezone

---
+++ spec/bdd/hello/internalionalization/anyone_can_change_their_timezone/change_timezone_on_the_profile_page_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe "Hello Gem", type: :feature do
+  goal_feature "Internationalization", "Anyone Can Change Their Timezone", "On The Profile Page" do
+
+    # ACCEPTANCE CRITERIA
+    # - 
+
+    sstory "As a User" do
+      sscenario "Success" do
+        skip
+      end
+    end
+
+  end
+end
---
+++ spec/bdd/hello/internalionalization/anyone_can_change_their_timezone/change_timezone_on_the_sign_up_form_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe "Hello Gem", type: :feature do
+  goal_feature "Internationalization", "Anyone Can Change Their Timezone", "On The Sign Up Form" do
+
+    # ACCEPTANCE CRITERIA
+    # - My account learns my browser’s timezone
+
+    sstory "As a Guest" do
+      sscenario "Success" do
+        skip
+      end
+    end
+
+  end
+end
