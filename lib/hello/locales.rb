f3baff31ab7823f3b98f5fed88549da63915c44d
James Pinto <thejamespinto@gmail.com>
2015-09-06 21:36:46 -0300

    moving a method to a file in which it makes more sense

--- lib/hello/locales.rb
+++ lib/hello/locales.rb
@@ -2,6 +2,10 @@ require 'hello/locales/locale_names'
 
 module Hello
 
+  def self.available_time_zones
+    ActiveSupport::TimeZone.send(:zones_map).values.map(&:name)
+  end
+
   def self.available_locales
     # yes, it would be better if this was not hardcoded
     # but we need this list somewhere so we can point out when a language is not supported by this gem
--- lib/hello/support.rb
+++ lib/hello/support.rb
@@ -1,9 +1,5 @@
 module Hello
 
-  def self.available_time_zones
-    ActiveSupport::TimeZone.send(:zones_map).values.map(&:name)
-  end
-
   def self.warning(s2)
     s1 = "HELLO DEV WARNING:".black.on_yellow.bold
     puts "#{s1} #{s2.yellow}"
