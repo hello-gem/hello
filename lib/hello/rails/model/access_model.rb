cd60995c0a87fbb14c2da4e48d26e5c1e32e451c
James Pinto <thejamespinto@gmail.com>
2015-10-07 22:36:48 -0300

    removing unused code

--- lib/hello/rails/model/access_model.rb
+++ lib/hello/rails/model/access_model.rb
@@ -2,29 +2,6 @@ module Hello
   module AccessModel
     extend ActiveSupport::Concern
 
-    # def full_device_name
-    #   obj = parsed_user_agent
-    #   a_browser = obj.to_s
-    #   a_os = obj.os.to_s
-    #   a_browser = "#{obj.name} #{obj.version && obj.version.major}".strip
-    #   a_os = "#{obj.os.name} #{obj.os.version && obj.os.version.major}".strip
-    #   a_device = obj.device.name
-
-    #   a_browser = a_browser.gsub("IE", "Internet Explorer") if a_browser.start_with? "IE"
-
-    #   if a_device == "Other"
-    #     "#{a_os} - #{a_browser}"
-    #   elsif a_device == "Spider"
-    #     "Spider: #{a_browser}"
-    #   else
-    #     "#{a_os} (#{a_device}) - #{a_browser}"
-    #   end.strip
-    # end
-
-    # def parsed_user_agent
-    #   Hello.user_agent_parser.parse(user_agent_string)
-    # end
-
     def full_device_name
       Hello::DeviceName.instance.parse(user_agent_string)
     end
