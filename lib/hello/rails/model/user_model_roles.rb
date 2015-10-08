bb25860306fbe63f5753a732b378511b1880d5b8
James Pinto <thejamespinto@gmail.com>
2015-10-07 21:57:35 -0300

    simplifying UserModelRoles

--- lib/hello/rails/model/user_model_roles.rb
+++ lib/hello/rails/model/user_model_roles.rb
@@ -1,73 +1,25 @@
 module Hello
   module UserModelRoles
-    extend ActiveSupport::Concern
-
-
-
-    # included do
-    # end
-
-    module ClassMethods
-
-      def mock_guest
-        new(role: 'guest')
-      end
-
-      def roles
-        # [guest, onboarding, user, webmaster]
-        [onboarding_role, user_role, webmaster_role]
-      end
-
-      # def guest
-      #   'guest'
-      # end
-
-      def onboarding_role
-        'onboarding'
-      end
-
-      def user_role
-        'user'
-      end
-
-      def webmaster_role
-        'webmaster'
-      end
-
-    end
-
-
 
     def guest?
-      role == 'guest'
+      %w(guest).include? role
     end
 
     def onboarding?
-      role == self.class.onboarding_role
+      %w(onboarding).include? role
     end
 
     def user?
-      webmaster? ||
-      role == self.class.user_role
+      %w(user webmaster).include? role
     end
 
     def webmaster?
-      role == self.class.webmaster_role
+      %w(webmaster).include? role
     end
 
     def role_is?(role)
       self.send("#{role}?")
     end
-
-
-
-    # def turn_onboarding_to_user!
-    #   # place additional code here
-    #   update! role: User.user_role
-    # end
-
-
-
 
   end
 end
