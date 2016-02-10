be1e25f54d9c177a032a1cf17e2ad18e03d92a12
James Pinto <thejamespinto@gmail.com>
2016-02-10 17:31:11 -0200

    fixing a small do-end in a spec

--- spec/others/localization_consistency_spec.rb
+++ spec/others/localization_consistency_spec.rb
@@ -27,43 +27,44 @@ module Hello
       expect(scandata).to eq([['bbb'], ['ddd']])
     end
 
-    describe 'Consistent with ENGLISH'
+    describe 'Consistent with ENGLISH' do
 
-    def consistency_wrap(locale, &_block)
-      h_lo = flat_i18n_hello(locale)
+      def consistency_wrap(locale, &_block)
+        h_lo = flat_i18n_hello(locale)
 
-      array_en = Array(h_en)
-      array_lo = Array(h_lo)
+        array_en = Array(h_en)
+        array_lo = Array(h_lo)
 
-      array_en.size.times do |i|
-        @en_key  = array_en[i][0]
-        en_val   = array_en[i][1]
+        array_en.size.times do |i|
+          @en_key  = array_en[i][0]
+          en_val   = array_en[i][1]
 
-        @lo_key  = array_lo[i][0]
-        lo_val   = array_lo[i][1]
+          @lo_key  = array_lo[i][0]
+          lo_val   = array_lo[i][1]
 
-        @en_vars = extract_string_replacement(en_val)
-        @lo_vars = extract_string_replacement(lo_val)
+          @en_vars = extract_string_replacement(en_val)
+          @lo_vars = extract_string_replacement(lo_val)
 
-        yield
-      end
-    end
-
-    available_locales = Dir[Hello::ROOT.join('config', 'locales', '**', '*.yml')].map { |s| s.split('.')[-2] }
-
-    available_locales.each do |locale|
-      describe "#{locale} consistency" do
-        it 'Keys are consistent' do
-          consistency_wrap(locale) do
-            error_message = "I18n '#{locale}' does not match 'en'.\nExpected key '#{@lo_key}' but found key '#{@en_key}'"
-            expect(@lo_key).to eq(@en_key), error_message
-          end
+          yield
         end
+      end
 
-        it 'Variables are consistent' do
-          consistency_wrap(locale) do
-            error_message = "I18n '#{locale}' does not match 'en'.\nExpected key '#{@lo_key}' with values '#{@en_vars}', but found '#{@lo_vars}' instead."
-            expect(@lo_vars).to eq(@en_vars), error_message
+      available_locales = Dir[Hello::ROOT.join('config', 'locales', '**', '*.yml')].map { |s| s.split('.')[-2] }
+
+      available_locales.each do |locale|
+        describe "#{locale} consistency" do
+          it 'Keys are consistent' do
+            consistency_wrap(locale) do
+              error_message = "I18n '#{locale}' does not match 'en'.\nExpected key '#{@lo_key}' but found key '#{@en_key}'"
+              expect(@lo_key).to eq(@en_key), error_message
+            end
+          end
+
+          it 'Variables are consistent' do
+            consistency_wrap(locale) do
+              error_message = "I18n '#{locale}' does not match 'en'.\nExpected key '#{@lo_key}' with values '#{@en_vars}', but found '#{@lo_vars}' instead."
+              expect(@lo_vars).to eq(@en_vars), error_message
+            end
           end
         end
       end
