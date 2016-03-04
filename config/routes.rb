a554253345641d0edec54eeffefdff4d1c7cae20
James Pinto <thejamespinto@gmail.com>
2016-03-04 17:18:28 -0300

    organizing routes.rb a bit

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,41 @@ Hello::Engine.routes.draw do
   root 'root#index'
 
   #
-  # EMAIL
+  # REGISTRATION
   #
 
   get 'sign_up' => 'email_sign_up#index'
   post 'sign_up' => 'email_sign_up#create'
   get 'sign_up/widget'  => 'email_sign_up#widget'
 
+  #
+  # AUTHENTICATION
+  #
+
+  resources :sessions, only: [:index, :new, :show, :destroy]
+
+  get 'sign_out' => 'sessions#sign_out'
+
   get 'sign_in'         => 'email_sign_in#index'
   post 'sign_in' => 'email_sign_in#authenticate'
+
+  get 'sudo_mode' => 'sudo_mode#form'
+  patch 'sudo_mode' => 'sudo_mode#authenticate'
+  get 'sudo_mode/expire' => 'sudo_mode#expire'
+
+  #
+  # MANAGEMENT
+  #
+
+  resources :accesses, only: [:index, :destroy]
+
+  resource :current_user, only: [:show, :update]
+
+  get 'cancel_account' => 'cancel_account#index'
+  post 'cancel_account' => 'cancel_account#cancel'
+
+  get 'locale' => 'locale#index'
+  post 'locale' => 'locale#update'
 
   resources :emails, only: [:index, :create, :destroy] do
     member do
@@ -21,18 +47,6 @@ Hello::Engine.routes.draw do
       get 'expired_confirmation_token' => 'confirm_emails#expired_confirmation_token'
     end
   end
-
-  #
-  # ACCOUNT MANAGEMENT
-  #
-  resource :current_user, only: [:show, :update]
-
-  get 'sudo_mode' => 'sudo_mode#form'
-  patch 'sudo_mode' => 'sudo_mode#authenticate'
-  get 'sudo_mode/expire' => 'sudo_mode#expire'
-
-  get 'cancel_account' => 'cancel_account#index'
-  post 'cancel_account' => 'cancel_account#cancel'
 
   resources :passwords, only: [:index, :show, :update] do
     collection do
@@ -47,17 +61,4 @@ Hello::Engine.routes.draw do
     end
   end
 
-  #
-  # AUTHENTICATION
-  #
-
-  resources :accesses, only: [:index, :destroy]
-  resources :sessions, only: [:index, :new, :show, :destroy]
-  get 'sign_out' => 'sessions#sign_out'
-
-  #
-  # LOCALE
-  #
-  get 'locale' => 'locale#index'
-  post 'locale' => 'locale#update'
 end
