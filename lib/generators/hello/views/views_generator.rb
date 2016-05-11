6848407a7e496ececaae11d434a63390a0633445
Marcelo Barreto <marcelobarretojunior@gmail.com>
2016-05-11 01:35:26 -0300

    Feature: Locales Generator

--- lib/generators/hello/views/views_generator.rb
+++ lib/generators/hello/views/views_generator.rb
@@ -1,14 +1,9 @@
+require 'hello'
+
 class Hello::ViewsGenerator < Rails::Generators::Base
-  # source_root File.expand_path('../templates', __FILE__)
-  source_root File.expand_path('../../../../../', __FILE__)
+  source_root Hello::ROOT
 
   def copy_the_views
     directory 'app/views/hello'
   end
-
-  def copy_the_locales
-    copy_file 'config/locales/hello.en.yml', 'config/locales/hello.en.yml'
-  end
-
-  protected
 end
